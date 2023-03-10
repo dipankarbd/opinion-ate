7552a05ea2182f58aaec702ef914436d9be42408
Dipankar Biswas <dipankar_cse@yahoo.com>
2023-03-10 15:42:27 +0600

    render list of restaurant

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,8 +1,14 @@
 import { useEffect } from 'react';
 
-export default function RestaurantList({ loadRestaurants }) {
+export default function RestaurantList({ loadRestaurants, restaurants }) {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
-  return <div>Restaurant List</div>;
+  return (
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+    </ul>
+  );
 }
